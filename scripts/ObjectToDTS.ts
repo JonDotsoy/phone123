99fed905595683441ee3bffa304e7582d1f59eff
Jonathan Delgado <hi@jon.soy>
2020-08-23 02:17:05 -0400

    feat: initialize commit

---
+++ scripts/ObjectToDTS.ts
@@ -0,0 +1,75 @@
+import { isObject, isArray } from 'util';
+import { Spacing } from './Spacing';
+
+class ObjectToDTS {
+    spacing = new Spacing();
+
+    private constructor() { }
+
+    println = (body: string | Buffer) => Buffer.from(`${this.spacing.toString()}${body}\n`);
+    print = (body: string) => Buffer.from(`${this.spacing.toString()}${body}`);
+    printStart = (body: string) => Buffer.from(`${body}\n`);
+
+    valueToBuffer = (val: any) => isObject(val) ? this.objectToBuffer(val) : Buffer.from(JSON.stringify(val));
+
+    propValueToBuffer = (prop: string, val: any): Buffer => {
+        if (isArray(val)) {
+            if (val.every(e => !isArray(e) && !isObject(e))) {
+                return Buffer.concat([
+                    this.println(`${JSON.stringify(prop)}: ${this.valueToBuffer(val)},`),
+                ]);
+            }
+        }
+        if (isObject(val)) {
+            return this.println(`${JSON.stringify(prop)}: ${this.valueToBuffer(val)},`);
+        }
+        return Buffer.concat([
+            this.println(`${JSON.stringify(prop)}: ${this.valueToBuffer(val)},`),
+        ]);
+    };
+
+    objectToBuffer = (obj: any): Buffer => {
+        if (isArray(obj)) {
+            if (obj.length === 0)
+                return Buffer.from('[]');
+            const bfs: Buffer[] = [];
+            bfs.push(this.printStart('['));
+            this.spacing.up();
+
+            const propsBfs: Buffer[] = [];
+
+            for (const val of obj) {
+                propsBfs.push(this.println(`${this.valueToBuffer(val)},`));
+            }
+
+            bfs.push(...propsBfs);
+            this.spacing.down();
+            bfs.push(this.print(']'));
+
+            return Buffer.concat(bfs);
+        }
+        if (isObject(obj)) {
+            const bfs: Buffer[] = [];
+
+            bfs.push(this.printStart('{'));
+            this.spacing.up();
+
+            const propsBfs: Buffer[] = [];
+
+            for (const [prop, val] of Object.entries(obj)) {
+                propsBfs.push(this.propValueToBuffer(prop, val));
+            }
+
+            bfs.push(...propsBfs);
+            this.spacing.down();
+            bfs.push(this.print('}'));
+
+            return Buffer.concat(bfs);
+        }
+        return this.println('null');
+    };
+
+    static transform = (obj: any) => new ObjectToDTS().objectToBuffer(obj)
+}
+
+export const objectToDTS = (obj: any) => ObjectToDTS.transform(obj);
---
+++ scripts/download_country_codes_countrycode.org.ts
@@ -0,0 +1,259 @@
+import fs from 'fs'
+import csvParse from 'csv-parse/lib/sync'
+import https from 'https'
+import { throws } from 'assert'
+import { objectToJS } from './ObjectToJS'
+import { objectToDTS } from './ObjectToDTS'
+
+const SOURCE_PATH = `${__dirname}/../src`
+const LIB_PATH = `${__dirname}/../lib`
+
+// https://countrycode.org/customer/countryCode/downloadCountryCodes
+// https://countrycode.org/customer/countryCode/downloadNationalCodes?country=AL
+// https://countrycode.org/customer/countryCode/downloadCityCodes?country=AL
+
+export interface NationalCode {
+    PhoneCode: string
+    Description: string
+}
+
+export interface CityCode {
+    PhoneCode: string
+    Description: string
+}
+
+export interface CountryCode {
+    CountryName: string
+    ISO2: string
+    ISO3: string
+    TopLevelDomain: string
+    FIPS: string
+    ISONumeric: number
+    GeoNameID: number
+    E164: number
+    PhoneCode: string[]
+    Continent: Continent
+    Capital: string
+    TimeZoneinCapital: string
+    Currency: string
+    LanguageCodes: string[]
+    Languages: string
+    AreaKM2: number
+    InternetHosts: number
+    InternetUsers: number
+    "Phones (Mobile)": number
+    "Phones (Landline)": number
+    GDP: number
+}
+
+export enum Continent {
+    Africa = "Africa",
+    Antarctica = "Antarctica",
+    Asia = "Asia",
+    Europe = "Europe",
+    NorthAmerica = "North America",
+    Oceania = "Oceania",
+    SouthAmerica = "South America",
+}
+
+export interface CountryCodeFull extends CountryCode {
+    NationalCodes: NationalCode[]
+    CityCodes: CityCode[]
+}
+
+const getCache = (cachePath?: string) => cachePath
+    ? {
+        has: () => fs.existsSync(cachePath),
+        get: () => fs.readFileSync(cachePath),
+        put: (buffer: Buffer) => fs.writeFileSync(cachePath, buffer),
+    }
+    : undefined
+
+const downloadBuffer = ({ url, cachePath }: { url: string, cachePath?: string }) => new Promise<Buffer>((resolve, reject) => {
+    const c = getCache(cachePath)
+
+    if (c?.has()) return resolve(c.get())
+
+    console.log(`Download... ${url}`)
+
+    const req = https
+        .get(url, (res) => {
+            const body: Buffer[] = []
+            res.on('data', (d: Buffer) => {
+                body.push(d)
+            })
+
+            res.on('end', () => {
+                const buffer = Buffer.concat(body)
+                c?.put(buffer)
+                resolve(buffer)
+            })
+
+            res.on('error', err => {
+                reject(err)
+            })
+        })
+
+    req.end()
+})
+
+const downloadCountryCodes = async (): Promise<CountryCode[]> => {
+    const res = await downloadBuffer({
+        url: 'https://countrycode.org/customer/countryCode/downloadCountryCodes',
+        cachePath: `${SOURCE_PATH}/countrycode.org/countrycodes.csv`,
+    })
+
+    return csvParse(res.toString('latin1'), {
+        fromLine: 2,
+        columns: [
+            "CountryName",
+            "ISO2",
+            "ISO3",
+            "TopLevelDomain",
+            "FIPS",
+            "ISONumeric",
+            "GeoNameID",
+            "E164",
+            "PhoneCode",
+            "Continent",
+            "Capital",
+            "TimeZoneinCapital",
+            "Currency",
+            "LanguageCodes",
+            "Languages",
+            "AreaKM2",
+            "InternetHosts",
+            "InternetUsers",
+            "Phones (Mobile)",
+            "Phones (Landline)",
+            "GDP"
+        ],
+        cast: (value, context) => {
+            switch (context.column) {
+                case 'LanguageCodes':
+                case 'PhoneCode':
+                    return value.split(/\,\s*/)
+                case 'ISONumeric':
+                case 'GeoNameID':
+                case 'E164':
+                case 'AreaKM2':
+                case 'InternetHosts':
+                case 'InternetUsers':
+                case 'Phones (Mobile)':
+                case 'Phones (Landline)':
+                case 'GDP':
+                    return value ? Number(value) : null
+                default:
+                    return value
+            }
+        }
+    })
+}
+
+
+const downloadNationalCodes = async ({ ISO2 }: { ISO2: string }): Promise<NationalCode[]> => {
+    const res = await downloadBuffer({
+        url: `https://countrycode.org/customer/countryCode/downloadNationalCodes?country=${ISO2}`,
+        cachePath: `${SOURCE_PATH}/countrycode.org/nationalcodes-${ISO2}.csv`,
+    })
+
+    return csvParse(res.toString('latin1'), {
+        fromLine: 2,
+        columns: [
+            "PhoneCode",
+            "Description"
+        ],
+    })
+}
+
+const downloadCityCodes = async ({ ISO2 }: { ISO2: string }): Promise<CityCode[]> => {
+    const res = await downloadBuffer({
+        url: `https://countrycode.org/customer/countryCode/downloadCityCodes?country=${ISO2}`,
+        cachePath: `${SOURCE_PATH}/countrycode.org/citycodes-${ISO2}.csv`,
+    })
+
+    return csvParse(res.toString('latin1'), {
+        fromLine: 2,
+        columns: [
+            "PhoneCode",
+            "Description"
+        ],
+    })
+}
+
+const downloadCountryCodesFull = async (opt?: { cachePath?: string }): Promise<Buffer> => {
+    const c = getCache(opt?.cachePath)
+
+    if (c?.has()) return c.get()
+
+    const e: CountryCodeFull[] = []
+
+    for (const countryCode of await downloadCountryCodes()) {
+        const countrycodes = {
+            ...countryCode,
+            CityCodes: (await downloadCityCodes({ ISO2: countryCode.ISO2 })).filter(e => !!e.Description),
+            NationalCodes: (await downloadNationalCodes({ ISO2: countryCode.ISO2 })).filter(e => !!e.Description),
+        }
+        e.push(countrycodes)
+    }
+
+    const res = Buffer.from(JSON.stringify(e))
+
+    c?.put(res)
+
+    return res
+}
+
+const writeLibItem = (opt: { fileName: string, varName: string, body: any }) => {
+    const fileName = opt.fileName
+    const varName = opt.varName
+    const body = opt.body
+
+    fs.writeFileSync(`${LIB_PATH}/${fileName}.json`, JSON.stringify(body, null, 2))
+    fs.writeFileSync(
+        `${LIB_PATH}/${fileName}.d.ts`,
+        Buffer.concat([
+            Buffer.from(`export declare const ${varName}: `),
+            objectToDTS(body),
+            Buffer.from(`;\n`),
+        ])
+    )
+    fs.writeFileSync(
+        `${LIB_PATH}/${fileName}.js`,
+        Buffer.concat([
+            Buffer.from(`const ${varName} = `),
+            objectToJS(body),
+            Buffer.from(`;\n`),
+            Buffer.from(`\n`),
+            Buffer.from(`exports.${varName} = ${varName};\n`),
+        ])
+    )
+}
+
+const run = async () => {
+    const bf = await downloadCountryCodesFull({ cachePath: `${SOURCE_PATH}/countrycode.org/countrycodes.json` })
+
+    const countrycodes: CountryCodeFull[] = JSON.parse(bf.toString())
+    const nextcountrycodes = countrycodes
+    // .filter(e => e.ISO2 === 'CL')
+
+    fs.writeFileSync(`${LIB_PATH}/countrycodes.json`, JSON.stringify(nextcountrycodes, null, 2))
+    
+    writeLibItem({
+        fileName: `countrycodes`,
+        varName: `countrycodes`,
+        body: nextcountrycodes,
+    })
+
+    for (const countrycode of nextcountrycodes) {
+        writeLibItem({
+            fileName: `countrycodes-${countrycode.ISO2}`,
+            varName: `countrycodes${countrycode.ISO2}`,
+            body: countrycode,
+        })
+    }
+
+    console.log('done')
+}
+
+run()
